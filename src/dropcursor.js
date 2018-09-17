ad558a0af34f6d39d5922c0ac0544eef74240568
Daniil Dotsev <dddotsev@gmail.com>
2018-09-17 15:58:18 +0300

    Add documentation to code

--- src/dropcursor.js
+++ src/dropcursor.js
@@ -5,6 +5,18 @@ import {Decoration, DecorationSet} from "prosemirror-view"
 const gecko = typeof navigator != "undefined" && /gecko\/\d/i.test(navigator.userAgent)
 const linux = typeof navigator != "undefined" && /linux/i.test(navigator.platform)
 
+// :: (options: ?Object) → Plugin
+// Create a plugin that, when added to a ProseMirror instance,
+// causes a decoration to show up at the drop position when something
+// is dragged over the editor.
+//
+//   options::- These options are supported:
+//
+//     color::? string
+//     The color of the cursor. Defaults to `black`.
+//
+//     width::? number
+//     The precise width of the cursor in pixels. Defaults to 1.
 export function dropCursor(options) {
   function dispatch(view, data) {
     view.dispatch(view.state.tr.setMeta(plugin, data))
