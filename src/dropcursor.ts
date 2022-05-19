b54c18e6b43f9964e13239be795eb03fcc4a6a23
Marijn Haverbeke <marijn@haverbeke.nl>
2022-05-19 12:48:20 +0200

    Regenerate readme from TypeScript types

--- src/dropcursor.ts
+++ src/dropcursor.ts
@@ -119,7 +119,7 @@ class DropCursorView {
     let pos = this.editorView.posAtCoords({left: event.clientX, top: event.clientY})
 
     let node = pos && pos.inside >= 0 && this.editorView.state.doc.nodeAt(pos.inside)
-    let disableDropCursor = node && (node.type.spec as any).disableDropCursor
+    let disableDropCursor = node && node.type.spec.disableDropCursor
     let disabled = typeof disableDropCursor == "function" ? disableDropCursor(this.editorView, pos) : disableDropCursor
 
     if (pos && !disabled) {
